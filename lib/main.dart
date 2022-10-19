da6d00e7becf232020fe434dec0f3a3a52d71cad
danh_nguyen_232 <danhcathuy@gmail.com>
2022-10-19 07:26:01 +0700

    rebuild layout using new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,65 +12,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/danh00.jpg'),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: CircleAvatar(
+                    radius: 70.0,
+                    backgroundImage: AssetImage('images/danh00.jpg')
+                  ),
                 ),
                 Text(
                     'Nguyễn Quang Danh',
                     style: TextStyle(
+                      color: Colors.white,
                       fontFamily: 'Comfortaa',
-                      fontSize: 20.0,
+                      fontSize: 30.0,
                       fontWeight: FontWeight.bold
                     )
                 ),
                 Text(
                     'Flutter Developer'.toUpperCase(),
                     style: TextStyle(
-                      fontFamily: 'SourceSansPro'
+                      color: Colors.white,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      letterSpacing: 2.5
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                          '+84 79 2030 232'
-                      )
-                    ],
+                SizedBox(
+                  height: 20.0,
+                  width: 200.0,
+                  child: Divider(
+                    color: Colors.deepPurple.shade100,
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                          Icons.email
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                          'danhcathuy@gmail.com',
-                        style: TextStyle(
-                          fontFamily: 'SourceSansPro'
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                      ListTile(
+                        leading: Icon(Icons.phone,
+                          color: Colors.deepPurple
+                        ),
+                        title: Text('+84 79 2030 232',
+                          style: TextStyle(
+                            color: Colors.deepPurple
+                          ),
                         ),
                       )
-                    ],
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                        ListTile(
+                          leading: Icon(Icons.email,
+                            color: Colors.deepPurple
+                          ),
+                          title: Text('danhcathuy@gmail.com',
+                              style: TextStyle(
+                                fontFamily: 'SourceSansPro',
+                                color: Colors.deepPurple
+                              ),
+                          ),
+                        ),
                   ),
                 ),
               ],
